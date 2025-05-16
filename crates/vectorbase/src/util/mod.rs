2c9ac82d7fb7d7ca42bbe226fa68d698dbe3565b
szuwgh <895340293@qq.com>
2025-05-16 22:01:49 +0800

    remove vectorbase

--- crates/vectorbase/src/util/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod asyncio;
-pub(crate) mod bloom;
-pub(crate) mod common;
-pub mod error;
-pub mod fs;
-pub(crate) mod fst;
-pub(crate) mod index;
-pub mod time;
--- crates/vectorbase/src/util/time.rs
+++
@@ -1,9 +0,0 @@
-use chrono::Local;
-
-pub struct Time;
-
-impl Time {
-    pub fn now() -> i64 {
-        Local::now().timestamp()
-    }
-}
