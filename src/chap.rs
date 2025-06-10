e15c7383ad2a7fd0bf9a774ae596f07589ea7554
root <root@wgh.localdomain>
2025-06-10 18:19:19 +0800

    Modify tui

--- src/chap.rs
+++ src/chap.rs
@@ -11,14 +11,14 @@ use std::path::Path;
 use std::path::PathBuf;
 use tokio::runtime::Builder;
 use tokio::sync::mpsc;
-use vectorbase::ann::AnnType;
-use vectorbase::collection::Collection;
-use vectorbase::config::ConfigBuilder;
-use vectorbase::schema::FieldEntry;
-use vectorbase::schema::Schema;
-use vectorbase::schema::TensorEntry;
-use vectorbase::schema::VectorEntry;
-use vectorbase::schema::VectorType;
+// use vectorbase::ann::AnnType;
+// use vectorbase::collection::Collection;
+// use vectorbase::config::ConfigBuilder;
+// use vectorbase::schema::FieldEntry;
+// use vectorbase::schema::Schema;
+// use vectorbase::schema::TensorEntry;
+// use vectorbase::schema::VectorEntry;
+// use vectorbase::schema::VectorType;
 
 const LLM_MODEL_DIR: &'static str = "/etc/chappie/model";
 const CHAP_VB_DIR: &'static str = "/etc/chappie/data";
@@ -37,7 +37,7 @@ pub(crate) static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
 //app
 pub(crate) struct Chappie {
     tui: ChapTui,
-    vdb: Option<Collection>,
+    //vdb: Option<Collection>,
 }
 //
 impl Chappie {
@@ -59,51 +59,51 @@ impl Chappie {
         let llm_client = LlmClient::new(cli.get_llm(), cli.get_api_key(), cli.get_model())?;
         let (prompt_tx, prompt_rx) = mpsc::channel::<String>(1);
         let (llm_res_tx, llm_res_rx) = mpsc::channel::<String>(1);
-        let mut schema = Schema::with_vector(VectorEntry::new(
-            "vector",
-            AnnType::HNSW,
-            TensorEntry::new(1, [384], VectorType::F32),
-        ));
-        schema.add_field(FieldEntry::str("prompt"));
-        schema.add_field(FieldEntry::str("answer"));
-        let config = ConfigBuilder::default()
-            .data_path(PathBuf::from(CHAP_VB_DIR))
-            .collect_name("chap")
-            .build();
-        let vdb = if cli.get_vb() {
-            Some(Collection::new(schema.clone(), config)?)
-        } else {
-            None
-        };
+        // let mut schema = Schema::with_vector(VectorEntry::new(
+        //     "vector",
+        //     AnnType::HNSW,
+        //     TensorEntry::new(1, [384], VectorType::F32),
+        // ));
+        // schema.add_field(FieldEntry::str("prompt"));
+        // schema.add_field(FieldEntry::str("answer"));
+        // let config = ConfigBuilder::default()
+        //     .data_path(PathBuf::from(CHAP_VB_DIR))
+        //     .collect_name("chap")
+        //     .build();
+        // let vdb = if cli.get_vb() {
+        //     Some(Collection::new(schema.clone(), config)?)
+        // } else {
+        //     None
+        // };
         let chap_ui = ChapTui::new(
             cli.get_chap_mod(),
             prompt_tx,
-            vdb.clone(),
+            // vdb.clone(),
             //  embed.clone(),
             llm_res_rx,
             cli.get_ui_type(),
             cli.get_que(),
         )?;
-        let vb_c = vdb.clone();
-        Self::req_llm(prompt_rx, llm_res_tx, vb_c, schema, llm_client);
+        //  let vb_c = vdb.clone();
+        //Self::req_llm(prompt_rx, llm_res_tx, vb_c, schema, llm_client);
         Ok(Self {
             tui: chap_ui,
-            vdb: vdb,
+            //  vdb: vdb,
         })
     }
 
-    fn req_llm(
-        prompt_rx: mpsc::Receiver<String>,
-        llm_res_tx: mpsc::Sender<String>,
-        vdb: Option<Collection>,
-        //  embed_model: Arc<TextEmbedding>,
-        schema: Schema,
-        llm_client: LlmClient,
-    ) {
-        RUNTIME.spawn(async move {
-            request_llm(prompt_rx, llm_res_tx, vdb, schema, llm_client).await;
-        });
-    }
+    // fn req_llm(
+    //     prompt_rx: mpsc::Receiver<String>,
+    // //    llm_res_tx: mpsc::Sender<String>,
+    //     vdb: Option<Collection>,
+    //     //  embed_model: Arc<TextEmbedding>,
+    //   //  schema: Schema,
+    //     llm_client: LlmClient,
+    // ) {
+    //     RUNTIME.spawn(async move {
+    //         request_llm(prompt_rx, llm_res_tx, vdb, schema, llm_client).await;
+    //     });
+    // }
 
     pub(crate) fn run<P: AsRef<Path>>(&mut self, p: P) -> ChapResult<()> {
         RUNTIME.block_on(async move { self.tui.render(p).await })
@@ -122,38 +122,38 @@ impl Chappie {
     // }
 }
 
-async fn request_llm(
-    mut prompt_rx: mpsc::Receiver<String>,
-    llm_res_tx: mpsc::Sender<String>,
-    vdb: Option<Collection>,
-    // embed_model: Arc<TextEmbedding>,
-    schema: Schema,
-    mut llm_client: LlmClient,
-) {
-    let field_id_prompt = schema.get_field("prompt").unwrap();
-    loop {
-        let field_id_answer = schema.get_field("answer").unwrap();
-        tokio::select! {
-           Some(prompt) = prompt_rx.recv() => {
-            match llm_client.request(&prompt).await {
-                Ok(res)=>{
-                    // if let Some(vb) = &vdb {
-                    //     let embeddings = embed_model.embed(vec![&prompt], None).unwrap();
-                    //     let mut d = Document::new();
-                    //     d.add_text(field_id_prompt, &prompt);
-                    //     d.add_text(field_id_answer, &res);
-                    //     let v6 = Vector::from_slice(&embeddings[0], d).unwrap();
-                    //     // 插入向量数据库
-                    //     vb.add(v6).await.unwrap();
-                    // }
-                    llm_res_tx.send(res).await.unwrap();
-                }
-                Err(e)=>{
-                    llm_res_tx.send(e.to_string()).await.unwrap();
-                }
-            }
+// async fn request_llm(
+//     mut prompt_rx: mpsc::Receiver<String>,
+//     llm_res_tx: mpsc::Sender<String>,
+//     vdb: Option<Collection>,
+//     // embed_model: Arc<TextEmbedding>,
+//     schema: Schema,
+//     mut llm_client: LlmClient,
+// ) {
+//     let field_id_prompt = schema.get_field("prompt").unwrap();
+//     loop {
+//         let field_id_answer = schema.get_field("answer").unwrap();
+//         tokio::select! {
+//            Some(prompt) = prompt_rx.recv() => {
+//             match llm_client.request(&prompt).await {
+//                 Ok(res)=>{
+//                     // if let Some(vb) = &vdb {
+//                     //     let embeddings = embed_model.embed(vec![&prompt], None).unwrap();
+//                     //     let mut d = Document::new();
+//                     //     d.add_text(field_id_prompt, &prompt);
+//                     //     d.add_text(field_id_answer, &res);
+//                     //     let v6 = Vector::from_slice(&embeddings[0], d).unwrap();
+//                     //     // 插入向量数据库
+//                     //     vb.add(v6).await.unwrap();
+//                     // }
+//                     llm_res_tx.send(res).await.unwrap();
+//                 }
+//                 Err(e)=>{
+//                     llm_res_tx.send(e.to_string()).await.unwrap();
+//                 }
+//             }
 
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
--- src/vb/vectorbase.rs
+++ src/vb/vectorbase.rs
@@ -1,4 +1,2 @@
-
-
 //默认向量数据库
 struct DefaultVB {}
